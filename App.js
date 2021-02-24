137148640777f6b79df7a6b8a80c4d82aeda27e7
George Travis <wewerelikesharks@gmail.com>
2021-02-23 17:03:34 -0800

    front end successful connection to back end

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [title, setTitle] = useState();
+
+  const getTitle = () => {
+    axios
+      .get(`https://catalog-everything-backend.herokuapp.com/`)
+      .then((response) => {
+        setTitle(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getTitle();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Catalog Everything!</Text>
+      <Text>Welcome</Text>
+      <Text>{title ? title : `loading`}</Text>
       <StatusBar style="auto" />
     </View>
   );
